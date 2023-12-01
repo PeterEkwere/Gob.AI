1fd43d0e58b4cd4eaf06d5771af7574c8920d9a7
PeterEkwere <peterekwere@icloud.com>
2023-12-01 22:53:25 +0100

    Updated login

--- api/v1/views/login.py
+++ api/v1/views/login.py
@@ -9,15 +9,6 @@ from api.v1.views import app_views
 from flask import abort, jsonify, make_response, request
 from flasgger.utils import swag_from
 import json
-from os import getenv
-
-storage_type = getenv("GOBAI_TYPE_STORAGE")
-if storage_type == 'db':
-    from models.engine.db_storage import DBStorage
-    storage = DBStorage()
-else:
-    from models.engine.file_storage import FileStorage
-    storage = FileStorage()
 
 
 
